770dd19ee741bef31b83e54f220b738d7e36d0c3
Flolle <dyna87@gmail.com>
2025-09-04 17:00:26 +0200

    Update: Added enemy death animation and shader effects

--- features/bullet/Bullet.gd
+++ features/bullet/Bullet.gd
@@ -3,7 +3,7 @@ extends Area2D
 @onready var bullet_sprite_2d: Sprite2D = $Bullet_Sprite2D
 
 const SPEED: int = 500
-const DAMAGE: int = 20
+const DAMAGE: int = 30
 var direction: int = 1
 
 func _physics_process(_delta: float) -> void:
@@ -23,5 +23,9 @@ func _on_body_entered(body: Node2D) -> void:
 	# Sprite umdrehen, wenn nach links geschossen wird
 	if direction == -1:
 		vfxInstance.scale.x = -1
+		
+	var enemy = body as EnemyController
+	if enemy:
+		enemy.ApplyDamage(DAMAGE)
 	
 	queue_free() #Und wieder weg mit der Bullet
--- features/enemies/Enemy.gd
+++ features/enemies/Enemy.gd
@@ -1,8 +1,12 @@
 extends CharacterBody2D
+class_name EnemyController
 
 const SPEED: int = 50
 var direction: int = 1
 const GRAVITY: int = 1800
+
+var currentHealth: int = 90
+var isDeath: bool = false
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var ray_cast_2d_forward: RayCast2D = $CollisionShape2D/RayCast2D_Forward
@@ -16,6 +20,9 @@ func _physics_process(_delta: float) -> void:
 	if not is_on_floor():
 		velocity.y = GRAVITY
 	
+	if isDeath:
+		return
+	
 	#Wenn etwas vor dem Gegner ist oder der Abgrund bevorsteht:
 	if ray_cast_2d_forward.is_colliding() || ray_cast_2d_downward.is_colliding() == false:
 		direction = -direction #Und umkehren bitte
@@ -28,7 +35,30 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 	
 func updateAnimation():
+	if isDeath:
+		return
+		
 	if velocity.x != 0:
 		animated_sprite_2d.flip_h = velocity.x < 0 #Ergibt true oder false, dementsprechend wird "geflippt"
 	
 	animated_sprite_2d.play("Walk")
+	
+func ApplyDamage(damage: int):
+	#Er ist Tot Jim
+	if isDeath:
+		return
+	
+	currentHealth -= damage
+	
+	var blink_tween = get_tree().create_tween()
+	blink_tween.tween_method(UpdateBlink, 1.0, 0.0, 0.3)
+	
+	if currentHealth <= 0:
+		isDeath = true
+		animated_sprite_2d.play("Die")
+		set_collision_layer_value(3, false)
+		await  get_tree().create_timer(2).timeout
+		queue_free()
+		
+func UpdateBlink(newValue: float):
+	animated_sprite_2d.set_instance_shader_parameter("Blink", newValue)
